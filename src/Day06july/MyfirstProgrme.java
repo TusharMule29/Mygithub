3ab662326fe4eec1f2565dee86adb2f7ffcfa9fb
TusharMule29 <tushar.mule.96@gmail.com>
2023-07-06 12:55:14 +0530

    first commit

---
+++ src/Day06july/MyfirstProgrme.java
@@ -0,0 +1,16 @@
+package Day06july;
+
+public class MyfirstProgrme {
+
+	public static void main(String[] args) {
+
+		int a =10;
+		String my = " tushar";
+		
+		System.out.println(a);
+		
+		
+		
+	}
+
+}
